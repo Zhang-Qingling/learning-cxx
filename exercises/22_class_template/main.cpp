7735a142c0c97b31401ad10f19ab515b66c9e71e
secondtonone1 <secondtonone1@163.com>
2026-01-19 20:11:56 +0800

    finish all exercises

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (auto i = 0; i < 4; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,24 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        for (int d = 0; d < 4; ++d) {
+        ASSERT(others.shape[d] == 1 || others.shape[d] == shape[d],
+               "shape mismatch for broadcasting");
+        }
+        for (auto i = 0u; i < shape[0]; ++i) {
+            for (auto j = 0u; j < shape[1]; ++j) {
+                for (auto k = 0u; k < shape[2]; ++k) {
+                    for (auto l = 0u; l < shape[3]; ++l) {
+                        auto o_i = others.shape[0] == 1 ? 0 : i;
+                        auto o_j = others.shape[1] == 1 ? 0 : j;
+                        auto o_k = others.shape[2] == 1 ? 0 : k;
+                        auto o_l = others.shape[3] == 1 ? 0 : l;
+                        data[((i * shape[1] + j) * shape[2] + k) * shape[3] + l] +=
+                            others.data[((o_i * others.shape[1] + o_j) * others.shape[2] + o_k) * others.shape[3] + o_l];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
